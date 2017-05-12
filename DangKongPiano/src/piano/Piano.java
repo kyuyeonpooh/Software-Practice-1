cdb17cafe9460a4ec40b48dd974d5587e8da3d2e
samsung <samsung@DESKTOP-NK3IBEN>
2017-05-12 23:07:56 +0900

    ><

--- DangKongPiano/src/piano/Piano.java
+++ DangKongPiano/src/piano/Piano.java
@@ -20,6 +20,7 @@ public class Piano {
         try {
           Piano piano = new Piano();
           piano.frame.setVisible(true);
+ 
         } catch (Exception e) {
           e.printStackTrace();
         }
@@ -37,7 +38,7 @@ public class Piano {
 
   public void setFrame() {
     frame = new JFrame();
-    frame.setBounds(100, 100, 653, 372);
+    frame.setBounds(100, 100, 1100, 372);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(false);
     frame.setTitle("DangKong Piano");
@@ -53,7 +54,9 @@ public class Piano {
       "A0", "B0Flat", "B0", "C1", "C1Sharp",
       "D1", "E1Flat", "E1", "F1", "F1Sharp",
       "G1", "A1Flat", "A1", "B1Flat", "B1",
-      "C2"
+      "C2", "C2Sharp", "D2", "E2Flat", "E2",
+      "F2", "F2Sharp", "G2", "A2Flat", "A2",
+      "B2Flat", "B2"
     };
   }
   
@@ -66,7 +69,9 @@ public class Piano {
       KeyEvent.VK_Q, KeyEvent.VK_2, KeyEvent.VK_W, KeyEvent.VK_E, KeyEvent.VK_4,
       KeyEvent.VK_R, KeyEvent.VK_5, KeyEvent.VK_T, KeyEvent.VK_Y, KeyEvent.VK_7,
       KeyEvent.VK_U, KeyEvent.VK_8, KeyEvent.VK_I, KeyEvent.VK_9, KeyEvent.VK_O,
-      KeyEvent.VK_P
+      KeyEvent.VK_Z, KeyEvent.VK_S, KeyEvent.VK_X, KeyEvent.VK_D, KeyEvent.VK_C,
+      KeyEvent.VK_V, KeyEvent.VK_G, KeyEvent.VK_B, KeyEvent.VK_H, KeyEvent.VK_N,
+      KeyEvent.VK_J, KeyEvent.VK_M
     };
   }
   
@@ -79,7 +84,9 @@ public class Piano {
       25, 52, 83, 141, 169,
       199, 226, 257, 315, 341,
       373, 399, 430, 457, 488,
-      546
+      546, 573, 604, 631, 662,
+      720, 747, 778, 805, 836,
+      863, 894
     };
   }
   
