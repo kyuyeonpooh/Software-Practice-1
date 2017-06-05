d9a6dc7f45064a284e5c22dcec346dc63b333802
numpoop <lion721721@gmail.com>
2017-06-06 03:33:07 +0900

    Play DangKong

--- DangKongPiano/src/songPractice/PlayMusic.java
+++ DangKongPiano/src/songPractice/PlayMusic.java
@@ -1,8 +1,19 @@
 package songPractice;
 
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
 import piano.Piano;
+import piano.Key.ColorEffect;
+
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -15,6 +26,7 @@ public class PlayMusic extends JPanel {
 
   private static boolean isOn = false;
 
+  private AbstractAction pressed;
   private Piano piano;
   private String song;
   private BufferedReader br;
@@ -25,21 +37,87 @@ public class PlayMusic extends JPanel {
     setPiano();
     setSong(song);
     getSong();
+    readLines();
   }
-
-  void readLine() throws IOException {
+  
+  void readLines(){
+	try {
+		int len = readLine();
+		listen(len);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+  }
+  
+  void listen(int len){
+//	setPressed();
+//	setButton(keyCode, xBound);
+  }
+/*
+  public void setPressed() {
+	    this.pressed = new AbstractAction() {
+	      @Override
+	      public void actionPerformed(ActionEvent evt) {
+	        try {
+	          Thread effect = new ColorEffect();
+	          effect.start();
+	        } catch (IOException e) {
+	          e.printStackTrace();
+	        }
+	      }
+	    };
+  }
+  
+  public void setButton(int keyCode, int xBound) {
+	    this.button = new JButton();
+	    this.button.addActionListener(this.pressed);
+	    this.button.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(keyCode, 0), this.melody);
+	    this.button.getActionMap().put(this.melody, this.pressed);
+	    this.button.setBackground(this.color);
+	    if (this.color == Color.BLACK)
+	      this.button.setBounds(xBound, 12, 60, 90);
+	    else
+	      this.button.setBounds(xBound, 101, 60, 200);
+}
+  
+  private class ColorEffect extends Thread {
+	    @Override
+	    public void run() {
+	      button.setBackground(Color.GRAY);
+	      try {
+	        Thread.sleep(200);
+	        button.setBackground(color);
+	      } catch (InterruptedException e) {
+	        e.printStackTrace();
+	      }
+	    }
+	  }
+  */
+  int readLine() throws IOException {
     String data = br.readLine();
-    
+    int len = data.length();
+    JLabel[] text = new JLabel[len];
+    for(int i = 0; i<len ; i++){
+    	setLetter(text[i], i, data.charAt(i));
+    }
+    return len;
   }
 
+  void setLetter(JLabel lbl, int xpos, char letter){
+	String txt=Character.toString(letter);
+	lbl = new JLabel(txt);
+	lbl.setFont(new Font("Comic Sans MS", Font.BOLD, 60));
+	lbl.setBounds(xpos*100+100, 25, 60, 60);
+	add(lbl);
+  }
+  
   void getSong(){
     InputStream fis;
 	try {
-		fis = new FileInputStream("./resource/music/" + this.song + ".csv");
+		fis = new FileInputStream("./resource/music/" + this.song + ".txt");
 	    Reader isr = new InputStreamReader(fis);
 	    br = new BufferedReader(isr);
 	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
   }
